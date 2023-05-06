3f361220509b384474c247683e925bc7e5cfa24c
Alex <olivesargreek@gmail.com>
2023-05-06 16:04:17 +0300

    feat: магия адаптива && магия swiper slider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import './index.html';
 import 'swiper/scss';
 import 'swiper/scss/pagination';
 import './index.scss';
+import { sliderInit } from './modules/sliders';
+
+//use modules
+
+sliderInit('.about__slider', {
+  pagination: {
+    el: '.about__slider-pagination',
+  }
+});
 
 const videoBG = document.querySelector('.video-bg');
+
 videoBG.innerHTML = `
   <source src="video/video.webm" type="video/webm">
   <source src="video/video.mp4" type="video/mp4">
